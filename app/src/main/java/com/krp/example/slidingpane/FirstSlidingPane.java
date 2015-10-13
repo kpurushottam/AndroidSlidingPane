ac8cf798957880018a03fbe6b630c52210a6c657
unknown <purushottam.kumar@user492.aepl.com>
2015-10-13 11:08:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/krp/example/slidingpane/FirstSlidingPane.java
@@ -0,0 +1,20 @@
+package com.krp.example.slidingpane;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by purushottam.kumar on 10/13/2015.
+ */
+public class FirstSlidingPane extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first_sliding_pane, container, false);
+    }
+}
